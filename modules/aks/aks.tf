be86b311985808dd5671a12842c01990f237ad9a
wuerzelchen <legionaer85@hotmail.com>
2019-12-09 21:18:06 +0100

    added aks as module

---
+++ modules/aks/aks.tf
@@ -0,0 +1,20 @@
+resource "azurerm_kubernetes_cluster" "test" {
+  for_each = toset(var.location)
+  name                = "${var.cluster_name}-${replace(each.value, " ", "")}"
+  location            = each.value
+  resource_group_name = "${var.rg_name}-${replace(each.value, " ", "")}"
+  dns_prefix          = "${var.cluster_dns_prefix}-${replace(each.value, " ", "")}"
+
+  agent_pool_profile {
+    name            = "default"
+    count           = 1
+    vm_size         = "Standard_D1_v2"
+    os_type         = "Linux"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+}
---
+++ modules/aks/vars.tf
@@ -0,0 +1,29 @@
+variable "client_id" {
+    description = "ClientId of the Service Principal"
+    type = string
+}
+
+variable "client_secret" {
+    description = "Secret of the Service Principal"
+    type = string
+}
+
+variable "location" {
+    description = "Location of the resource group"
+    type = list(string)
+}
+
+variable "rg_name" {
+    description = "Resource Group Name"
+    type = string
+}
+
+variable "cluster_name" {
+    description = "Name of the AKS Cluster"
+    type = string
+}
+
+variable "cluster_dns_prefix" {
+    description = "DNS Prefix of the Master API"
+    type = string
+}

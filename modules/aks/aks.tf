7de4ba3e616ed9b3189d5d52c10dfdb1a69aa252
wuerzelchen <legionaer85@hotmail.com>
2019-12-09 22:27:48 +0100

    fixed error with rg name

--- modules/aks/aks.tf
+++ modules/aks/aks.tf
@@ -2,7 +2,7 @@ resource "azurerm_kubernetes_cluster" "test" {
   for_each = toset(var.location)
   name                = "${var.cluster_name}-${replace(each.value, " ", "")}"
   location            = each.value
-  resource_group_name = "${var.rg_name}-${replace(each.value, " ", "")}"
+  resource_group_name = "${var.rg_name}"
   dns_prefix          = "${var.cluster_dns_prefix}-${replace(each.value, " ", "")}"
 
   agent_pool_profile {
